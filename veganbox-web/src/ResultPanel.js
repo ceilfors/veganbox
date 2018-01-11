059077c2fa2dc3849c3bedc4beffe96cff6a3a30
Wisen Tanasa <wisen@ceilfors.com>
2018-01-11 15:14:59 +0000

    Convert to stateless component

--- veganbox-web/src/ResultPanel.js
+++ veganbox-web/src/ResultPanel.js
@@ -2,25 +2,20 @@ import React, { Component } from 'react';
 
 import { Glyphicon } from 'react-bootstrap'
 
-class ResultPanel extends Component {
-
-    render() {
-        if (this.props.criteria.length === 0) {
-            return <div />
-        }
-
-        const ok = <div id="result-ok">
-            <Glyphicon glyph="ok" />
-            <span id="resultPanel" style={{marginLeft: '8px'}}>{`Good choice! Finish that ${this.props.criteria}`}</span>
-        </div>
-
-        const bad = <div id="result-bad">
-            <Glyphicon glyph="remove" />
-            <span id="resultPanel" style={{marginLeft: '8px'}}>{`${this.props.criteria} is a no no, find other food`}</span>
-        </div>
-
-        return this.props.result ? ok : bad
+export default ({criteria, result}) => {
+    if (criteria.length === 0) {
+        return <div />
     }
-}
 
-export default ResultPanel
+    const ok = <div id="result-ok">
+        <Glyphicon glyph="ok" />
+        <span id="resultPanel" style={{marginLeft: '8px'}}>{`Good choice! Finish that ${criteria}`}</span>
+    </div>
+
+    const bad = <div id="result-bad">
+        <Glyphicon glyph="remove" />
+        <span id="resultPanel" style={{marginLeft: '8px'}}>{`${criteria} is a no no, find other food`}</span>
+    </div>
+
+    return result ? ok : bad
+}
